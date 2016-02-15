594981c53edda95583ac0f6c62f832776b5f8075
Tim Dupree <tdupree@ifbyphone.com>
2016-02-15 17:14:00 -0600

    more problems

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 // nasty code that Hound should yell about
-var thing = "tim",
+var thing = "tim", it="q",
 
 foo = bar;
 
 function boogey(){
     if (true) thing.foo();
+
+    if (false) {
+
+    } else {
+        if (true) {
+
+        }
+    }
+
     return
 
 42;
